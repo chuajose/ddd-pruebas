bd62ad5d5dbd08759b37c415cc53929cf7e34912
Jose Manuel Suarez Bravo <chua.jose@gmail.com>
2019-01-09 23:46:11 +0100

    update travis composer

--- tests/Domain/User/ValueObject/EmailTest.php
+++ tests/Domain/User/ValueObject/EmailTest.php
@@ -8,7 +8,27 @@
 
 namespace App\Tests\Domain\User\ValueObject;
 
+use App\Domain\User\Model\UserId;
+use App\Domain\User\ValueObject\Auth\HashedPassword;
+use App\Domain\User\ValueObject\Email;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use App\Domain\User\Model\User;
 
-class EmailTest {
+class EmailTest extends TestCase{
+
+	/**
+	 * @test
+	 *
+	 * @group unit
+	 *
+	 * @throws \Exception
+	 * @throws \Assert\AssertionFailedException
+	 */
+	public function given_a_valid_email_it_should_create_a_valid_email(): void
+	{
+		$emailString = 'lol@aso.maximo';
+		$email = Email::fromString($emailString);
+		self::assertSame($emailString, $email->__toString());
+	}
 
 }
